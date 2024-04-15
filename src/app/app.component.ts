f9ab07b1a23a51f44749251679b127a802ac8e83
mohamedhamdy95 <mohamed.hamdy9995@gmail.com>
2024-04-15 16:52:11 +0200

    15/4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
-
-
+import { TableModule } from 'primeng/table';
+import { CardModule } from 'primeng/card';
+import { SplitButtonModule } from 'primeng/splitbutton';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +13,88 @@ import { ButtonModule } from 'primeng/button';
     CommonModule,
     RouterOutlet,
     ButtonModule,
-    
+    TableModule,
+    CardModule,
+    SplitButtonModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'primeTask';
+  
+  products:any[]=[
+    {
+      id: '1',
+      code: 'ds226dsds',
+      name: 'Bamboo Watch',
+      description: 'Product Description',
+      image: 'bamboo-watch.jpg',
+      price: 65,
+      category: 'Accessories',
+      quantity: 30,
+      inventoryStatus: 'INSTOCK',
+      rating: 5
+  },
+  {
+    id: '2',
+    code: 'f230fh0g3',
+    name: 'Smart Watch',
+    description: 'Product Description',
+    image: 'bamboo-watch.jpg',
+    price: 65,
+    category: 'Accessories',
+    quantity: 24,
+    inventoryStatus: 'INSTOCK',
+    rating: 5
+},
+  {
+    id: '3',
+    code: 'fghf66',
+    name: 'test',
+    description: 'Product Description',
+    image: 'bamboo-watch.jpg',
+    price: 65,
+    category: 'Accessories',
+    quantity: 60,
+    inventoryStatus: 'INSTOCK',
+    rating: 5
+},
+  {
+    id: '4',
+    code: 'fghf66',
+    name: 'test',
+    description: 'Product Description',
+    image: 'bamboo-watch.jpg',
+    price: 65,
+    category: 'Accessories',
+    quantity: 60,
+    inventoryStatus: 'INSTOCK',
+    rating: 5
+},
+  {
+    id: '5',
+    code: 'fghf66',
+    name: 'test',
+    description: 'Product Description',
+    image: 'bamboo-watch.jpg',
+    price: 65,
+    category: 'Accessories',
+    quantity: 60,
+    inventoryStatus: 'INSTOCK',
+    rating: 5
+},
+  {
+    id: '6',
+    code: 'sdfgdf6',
+    name: 'Bamboo ',
+    description: 'Product Description',
+    image: 'bamboo-watch.jpg',
+    price: 65,
+    category: 'Accessories',
+    quantity: 16,
+    inventoryStatus: 'INSTOCK',
+    rating: 5
+},
+  ];
 }
